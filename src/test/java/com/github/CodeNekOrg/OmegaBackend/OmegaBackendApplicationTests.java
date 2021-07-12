310f382a88f2b45086392a883b613a7fedbc639e
codenekomancer <codenekomancer@gmail.com>
2021-07-12 19:20:49 +0200

    Initial Commit

---
+++ src/test/java/com/github/CodeNekOrg/OmegaBackend/OmegaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.CodeNekOrg.OmegaBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmegaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
